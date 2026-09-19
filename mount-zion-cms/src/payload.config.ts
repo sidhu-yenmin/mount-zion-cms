6a1f93aa6376f44066b8ca600163455b06c8da76
infanta <silvia.yenmin@gmail.com>
2026-09-19 17:19:56 +0530

    menus completed

--- mount-zion-cms/src/payload.config.ts
+++ mount-zion-cms/src/payload.config.ts
@@ -10,6 +10,7 @@ import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
 import { Header } from './globals/Header'
 import { Footer } from './globals/Footer'
+import { Menu } from './globals/Menu'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -22,7 +23,7 @@ export default buildConfig({
     },
   },
   collections: [Users, Media, Pages],
-  globals: [Header, Footer],
+  globals: [Menu, Header, Footer],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
